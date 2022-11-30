1d35ef8bd13cbe0d8944c3fa87e8b33a4e6f92c2
Sakthivel Karthikeyan <32382572+myselfsakthivel@users.noreply.github.com>
2022-11-30 17:07:35 +0530

    Update alerter.js

--- alerter.js
+++ alerter.js
@@ -22,6 +22,8 @@ function alertInCelcius(farenheit) {
         // However, this code doesn't count failures!
         // Add a test below to catch this bug. Alter the stub above, if needed.
         alertFailureCount += 0;
+        
+        expect(returnCode).equals(200);
     }
 }
 
