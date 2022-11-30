75876eb69a244e57b0108d17770f3cff9a066778
Sakthivel Karthikeyan <32382572+myselfsakthivel@users.noreply.github.com>
2022-11-30 16:58:22 +0530

    Update misaligned.js

--- misaligned.js
+++ misaligned.js
@@ -3,14 +3,16 @@ const {expect} = require('chai')
 function print_color_map() {
     const majorColors = ["White", "Red", "Black", "Yellow", "Violet"];
     const minorColors = ["Blue", "Orange", "Green", "Brown", "Slate"];
+    const finalMap = {};
     for (let i = 0; i < majorColors.length; i++) {
         for (let j = 0; j < minorColors.length; j++) {
             console.log(`${i * 5 + j} | ${majorColors[i]} | ${minorColors[j]}`);
+            finalMap[i * 5 + j] = `${majorColors[i]} | ${minorColors[j]}`;
         }
     }
-    return majorColors.length * minorColors.length;
+    return finalMap;
 }
 
 result = print_color_map();
-expect(result).equals(25);
+console.log(result);
 console.log('All is well (maybe!)');
